6bf34889d54e6d931d6554e58c4be4d8beeded28
Leif <leif@lightpalettestudios.com>
2019-09-12 23:51:30 -0600

    checking out google tags

---
+++ Springweb/js/simpletix-addtocart.js
@@ -0,0 +1,6 @@
+document.querySelector("#btnCheckout").classList.contains("disabled");
+
+document.querySelector("#frmOrder > div.modal-header.form__head > h3")
+  .innerText; //Title of the show
+
+document.querySelector("#ShowId").value; // Simpletix Show ID
---
+++ Springweb/js/simpletix-checkout.js
@@ -0,0 +1,5 @@
+document.querySelector("#GrandTotal").innerHTML.substr(1); //grand total
+document.querySelector("#hdnorderId").value; //order id
+document.querySelector(
+  "#dvMagnificentOrderSummary > div > div.seat__body > div > div.seat__group.ticket-request > ul > li > div.divListTickets > p"
+).innerHTML; //quantity and ticket type "2 X Sadonna  2019-10-03 8:00 PM Section: General "
